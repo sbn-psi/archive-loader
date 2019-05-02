4f5533c7df3a0d06ca26844a0c41a67c3bdf6d45
mdrum <mdrum@psi.edu>
2019-05-01 17:00:42 -0700

    first cut at archive load form

---
+++ solrize.js
@@ -0,0 +1,49 @@
+const isDict = function(item) {
+    return (!!item) && (item.constructor === Object)
+}
+const isDictList = function(item) {
+    return (!!item) && (item.constructor === Array) && isDict(item[0])
+}
+
+const solrizeDocument = function(doc, name) {
+    let docs = []
+    if (isDict(doc)) {
+        docs.push(solrizeNode("", name, doc))
+    }
+    if (isDictList(doc)) {
+        for (key in Object.keys(doc)) {
+            docs.push(solrizeNode("", name, doc[key]))
+        }
+    }
+    return docs
+}
+
+const solrizeNode = function(parentPath, name, node) {
+    let path = parentPath ? `${parentPath}.${name}` : name
+    let doc = {
+        attrname: name,
+        attrpath: path
+    }
+    let childDocs = []
+
+    for (const [key, value] of Object.entries(node)) {
+        if (isDict(value)) {
+            childDocs.push(solrizeNode(path, key, value))
+        }
+        else if(isDictList(value)) {
+            for (element of value) {
+                childDocs.push(solrizeNode(path, key, element))
+            }
+        }
+        else {
+            doc[key] = value
+        }
+    }
+
+    if (childDocs.length > 0) {
+        doc["_childDocuments_"] = childDocs
+    }
+    return doc
+}
+
+module.exports = solrizeDocument
