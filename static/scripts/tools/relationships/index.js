6254a0aa04421c6925b48d9f32eb52a7c3742843
Conor Kingston <cmkingston21@me.com>
2019-10-04 14:40:58 -0700

    new tools tab contains link to managing target relationships

---
+++ static/scripts/tools/relationships/index.js
@@ -0,0 +1,32 @@
+app.config(function($stateProvider) {
+    $stateProvider.state({
+        name: 'tools',
+        url: '/Tools',
+        redirectTo: 'tools.relationships'
+    })
+    $stateProvider.state({
+        name: 'tools.relationships',
+        url: '/Relationships',
+        templateUrl: 'states/tools/relationships.html',
+        data: {
+            title: 'Manage Relationships'
+        },
+        resolve: {
+            types: function($http) {
+                return $http.get('/relationship-types/target').then(res => res.data);
+            }
+        },
+        controller: function ManageRelationshipsController($scope, types) {
+            $scope.relationshipTypes = types;
+            
+            $scope.saveRelationships = function(rels) {
+                const newrels = rels.sort((rel1,rel2) => rel1.order > rel2.order).map((rel,idx) => {
+                    rel.order = idx + 1;
+                    return rel;
+                });
+                console.log(newrels);
+                // TODO: UPDATE TARGET RELATIONSHIPS
+            };
+        }
+    })
+})
