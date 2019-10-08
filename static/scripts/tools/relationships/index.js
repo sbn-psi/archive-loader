464552e4fe7b9adfdc540cf9957a600ebe96c694
Conor Kingston <cmkingston21@me.com>
2019-10-08 12:47:38 -0700

    instrument/target forms have separate loading spinners

--- static/scripts/tools/relationships/index.js
+++ static/scripts/tools/relationships/index.js
@@ -39,6 +39,8 @@ app.config(function($stateProvider) {
                 instrumentTypes: instrumentTypes,
                 getTargetRelationships: getTargetRelationships,
                 getInstrumentRelationships: getInstrumentRelationships,
+                savingTargets: false,
+                savingInstruments: false,
             }
         }
     })
