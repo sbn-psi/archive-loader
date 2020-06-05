94c948a3f328ee0cf17900055ae779d231f768f8
mdrum <mdrum@psi.edu>
2020-06-05 11:47:43 -0700

    replace request library with got part 2

--- server/httpRequest.js
+++ server/httpRequest.js
@@ -5,11 +5,10 @@ module.exports = async function httpRequest(baseUrl, params, body, username, pas
     };
     if(!!params) { options.searchParams = params }
     if(!!body) { 
-        options.body = body
+        options.json = body
         options.method = 'POST'
      }
     if(!!username) { options.username = username }
     if(!!password) { options.password = password }
-    console.log(options)
     return await request(baseUrl, options).json()
 }
